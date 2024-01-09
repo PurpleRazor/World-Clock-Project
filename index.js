d2db551653336e970e55b1a822bdfe81c35e4565
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2024-01-09 20:31:49 +0000

    Started Javascript

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+alert("hello");
 
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+
+sydneyDateElement.innerHTML = moment().format("mmmm Do YYYY");
+sydneyTimeElement = "1:49:15 <small>AM</small>";
